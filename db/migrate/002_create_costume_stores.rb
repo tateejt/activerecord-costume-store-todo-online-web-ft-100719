bc254c541808b59da10c2d5a70b9dcff316f2fa7
Emery Tate <tateejt@gmail.com>
2019-11-18 16:35:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name 
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees
+      t.integer :num_of_employees
       t.boolean :in_business
       t.string :opening_time
       t.string :closing_time
