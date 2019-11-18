24fa8de0077aa3dae3edd9ce1f91863a4b4b6e97
Emery Tate <tateejt@gmail.com>
2019-11-18 16:28:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,14 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change 
-    
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :in_business
+      t.string :opening_time
+      t.string :closing_time
+    end
   end
 end
